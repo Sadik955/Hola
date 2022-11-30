d1657247e1808751dd5d72738908179f7c7816cd
SADIK\SADIK <sadikshaik0405@gmail.com>
2022-11-30 15:17:05 +0530

    Halo project first commit

---
+++ Hola.java
@@ -0,0 +1,18 @@
+Class Hola
+{
+Public static void main(string args[])
+{
+System.out.println("WELCOME TO GIT")
+}
+}
+
+
+
+
+echo "# Hola" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Sadik955/Hola.git
+git push -u origin main
